bafdf5de07b41b05be5a1a2a78ecafa4f9466e7d
Darey <horla@Dareys-MacBook-Pro.local>
2020-05-20 23:12:55 +0100

    newest

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -2,6 +2,3 @@
 
 declare module "react-elastic-carousel";
 declare module "react-router-dom";
-declare module "react-image-appear";
-declare module "react-lazy-load";
-declare module "cloudinary-react";
