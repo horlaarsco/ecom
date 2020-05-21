faef010e29ee450507e96227a5e1500ed046ecfb
Darey <horla@Dareys-MacBook-Pro.local>
2020-05-21 05:45:57 +0100

    sswmw

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import { ApolloProvider } from "@apollo/react-hooks";
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000/",
+  uri: "https://horla-ecom.herokuapp.com/",
 });
 
 ReactDOM.render(
